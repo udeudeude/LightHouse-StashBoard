687412fecbc0b7314edfaa12c8bba97aba0ac1e9
udeudeude <udeudeude@gmail.com>
2026-09-04 22:54:52 -0400

    test: cover tip and stand geometry

--- test/application/board_controller_test.dart
+++ test/application/board_controller_test.dart
@@ -41,4 +41,22 @@ void main() {
       IlluminationPattern.full,
     );
   });
+
+  test('eastward drag tips upright element and reverse hinge drag stands it', () {
+    final controller = BoardController();
+    controller.createAt(const PhysicalPoint(50, 50));
+    final upright = controller.state.elements.single;
+
+    controller.tipOrStand(upright, const PhysicalPoint(10, 0));
+    final flat = controller.state.elements.single;
+    expect(flat.pose, PyramidPose.flat);
+    expect(flat.headingDegrees, closeTo(90, 0.001));
+    expect(flat.position.xMm, greaterThan(50));
+
+    controller.tipOrStand(flat, const PhysicalPoint(-10, 0));
+    final stood = controller.state.elements.single;
+    expect(stood.pose, PyramidPose.upright);
+    expect(stood.position.xMm, closeTo(50, 0.001));
+    expect(stood.position.yMm, closeTo(50, 0.001));
+  });
 }
