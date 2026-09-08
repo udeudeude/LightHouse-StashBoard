c43d37b440c3178c80575e275cca6831700ad0ff
udeudeude <udeudeude@gmail.com>
2026-09-08 18:40:53 -0400

    android: expose reported physical display dpi

--- android/app/src/main/kotlin/com/udeudeude/lighthouse/MainActivity.kt
+++ android/app/src/main/kotlin/com/udeudeude/lighthouse/MainActivity.kt
@@ -1,5 +1,28 @@
 package com.udeudeude.lighthouse
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "lighthouse/display",
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "physicalDpi" -> {
+                    val metrics = resources.displayMetrics
+                    result.success(
+                        mapOf(
+                            "xdpi" to metrics.xdpi.toDouble(),
+                            "ydpi" to metrics.ydpi.toDouble(),
+                        ),
+                    )
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
